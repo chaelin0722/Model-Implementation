a22b4530814007f4a303d54f7ccd77979778f0a6
chaelin0722 <ccaa9697@gmail.com>
2021-07-13 14:00:28 +0900

    googlenet fixed code 0713

--- googlenet_training_imagenet.py
+++ googlenet_training_imagenet.py
@@ -11,6 +11,7 @@ import numpy as np
 from PIL import Image
 from keras.utils import np_utils
 import math
+from keras.optimizers import SGD
 
 
 def _parse_tfrecord():
@@ -154,7 +155,7 @@ def main():
 
     outputs = Dense(1000, activation="softmax", name='main_classifier')(x)
 
-    concat_output = tf.keras.layers.concatenate([outputs, ax1, ax2])
+    concat_output = tf.keras.layers.concatenate([outputs*1.0, ax1*0.3, ax2*0.3])
 
     model = tf.keras.models.Model(inputs=input_data, outputs=concat_output, name='googlenet')
   # model.summary()
@@ -163,18 +164,12 @@ def main():
     learning_rate = 0.0001
     momentum = 0.9
 
-    optimizer = tf.keras.optimizers.SGD(lr=learning_rate, momentum=momentum, nesterov=False)
-    loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+    #optimizer = tf.keras.optimizers.SGD(lr=learning_rate, momentum=momentum, nesterov=False)
+    sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
+
 
     #model.compile(loss=loss, optimizer=optimizer, metrics=['accuracy'])
-    model.compile(optimizer=optimizer,
-                   loss={'main_classifier' : loss,
-                         'ax1' : loss,
-                         'ax2' : loss},
-                   loss_weights={'main_classifier': 1.0,
-                         'ax1': 0.3,
-                         'ax2': 0.3},
-                   metrics=['accuracy'])
+    model.compile(optimizer=sgd, loss='sparse_categorical_crossentropy', metrics=['acc'])
 
     checkpoint_path = "checkpoints/cp.ckpt"
     checkpoint_dir = os.path.dirname(checkpoint_path)
@@ -213,7 +208,6 @@ def main():
 
 if __name__ == '__main__':
     main()
-
 
 
 
