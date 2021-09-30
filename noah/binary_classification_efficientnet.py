a5255e2081c580acb36840436389852ad3164998
zzennin <53431568+chaelin0722@users.noreply.github.com>
2021-09-30 22:46:19 +0900

    update file and has error
    callback error... 버전 차이인지 뭔지 잘 모르겠다

--- noah/binary_classification_efficientnet.py
+++ noah/binary_classification_efficientnet.py
@@ -5,61 +5,48 @@ import cv2
 import matplotlib.pyplot as plt
 from keras import applications
 from efficientnet.keras import EfficientNetB3
+import keras
 from keras import callbacks
 from keras.models import Sequential
 from keras.preprocessing.image import ImageDataGenerator
 from keras.layers import Dense
 from keras.optimizers import Adam
+from keras.callbacks import ModelCheckpoint
 import tensorflow as tf
-import glob, warnings
+import datetime
+
+#위 행(line)은 runs/logs 폴더를 생성합니다.
+
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ['CUDA_VISIBLE_DEVICES'] = "0, 1"
 
 
 
-filenames = os.listdir("./test_dataset/")
-train_dir = './test_dataset'
+data_filenames = os.listdir("./arbitrary_labeled_dataset/")
+data_dir = './arbitrary_labeled_dataset/'
 
 
-TEST_IMAGES = glob.glob(train_dir + '/*.jpg')
 
-print(TEST_IMAGES)
 categories = []
-for filename in filenames:
+for filename in data_filenames:
     category = filename.split('_')[0]
-    if category == 'abnormal':
+    if category == 'error':
         categories.append(1)
     else:
         categories.append(0)
 
 df = pd.DataFrame({
-    'filename': filenames,
+    'filename': data_filenames,
     'category': categories
 })
 
-'''
-normal = []
-
-for i in range(len(filenames)):
-    normal.append(cv2.imread(train_dir + '/' + df['filename'][i]))
-
-
-labels = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-
-plt.figure(figsize=[10,10])
-for x in range(0,6):
-    plt.subplot(3, 3, x+1)
-    plt.imshow(normal[x])
-    plt.title(labels[x])
-    x += 1
-
-#plt.show()
-'''
-
 df['category'] = df['category'].astype('str')
-print(df)
 
-train_datagen = ImageDataGenerator(
+
+
+datagen = ImageDataGenerator(
     rescale=1/255,
-   # validation_split=0.10,
+    validation_split=0.10,
     rotation_range=40,
     width_shift_range=0.2,
     height_shift_range=0.2,
@@ -69,30 +56,31 @@ train_datagen = ImageDataGenerator(
     fill_mode='nearest'
 )
 
-print("df : ",df)
-train_generator = train_datagen.flow_from_dataframe(
+# print("df : ",df)
+train_generator = datagen.flow_from_dataframe(
     dataframe = df,
-    directory = train_dir,
+    directory = data_dir,
     x_col="filename",
     y_col="category",
     target_size=(32,32),
     subset="training",
-    batch_size=1024,
+    batch_size=128,
     shuffle=True,
     class_mode="binary"
 )
 
-val_generator = train_datagen.flow_from_dataframe(
+val_generator = datagen.flow_from_dataframe(
     dataframe = df,
-    directory = train_dir,
+    directory = data_dir,
     x_col="filename",
     y_col="category",
     target_size=(32,32),
     subset="validation",
-    batch_size=256,
+    batch_size=64,
     shuffle=True,
     class_mode="binary"
 )
+
 efficient_net = EfficientNetB3(
     weights='imagenet',
     input_shape=(32,32,3),
@@ -109,12 +97,31 @@ model.summary()
 
 model.compile(optimizer=Adam(lr=0.0001), loss='binary_crossentropy', metrics=['accuracy'])
 
+filename = './checkpoints/efficient-weights.h5'
+log_dir = "./logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+
+keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1),
+    # 개선된 validation score를 도출해낼 때마다 weight를 중간 저장
+cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=filename,
+                    save_weights_only=True,
+                    verbose=1,  # 로그를 출력
+#                    save_best_only=True,  # 가장 best 값만 저장
+                    mode='auto')  # auto는 알아서 best를 찾습니다. min/max
+
+
+steps_per_epochs = int((len(df) * 0.9) // train_generator.batch_size)
+step_size_valid = int((len(df) * 0.1) // val_generator.batch_size)
+
+#print(steps_per_epochs)  # 18
+#print(step_size_valid)   # 4
+
 history = model.fit_generator(
     train_generator,
-    epochs = 1,
-    steps_per_epoch = 15,
+    epochs = 50,
+    steps_per_epoch = steps_per_epochs,
     validation_data = val_generator,
-    validation_steps = 7
+    validation_steps = 7,
+    callbacks = [cp_callback],
 )
 
 acc = history.history['accuracy']
